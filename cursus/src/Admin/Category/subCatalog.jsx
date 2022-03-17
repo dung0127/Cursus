a4a3a3c6e43210159bad858792ded52104fb1ec4
dung0127 <44612274+dung0127@users.noreply.github.com>
2022-03-17 11:48:19 +0700

    clear input subcatalog

--- cursus/src/Admin/Category/subCatalog.jsx
+++ cursus/src/Admin/Category/subCatalog.jsx
@@ -36,6 +36,12 @@ class SubCatalog extends React.Component {
         this.props.createSubCatalogRequest(addSubCatalog);
         this.props.fetchSubCatalogRequest();
         this.props.fetchCatalogRequest();
+        Array.from(document.querySelectorAll('.sub')).forEach(input=>(input.value=""))
+        this.setState({addSubCatalog: {
+            catalogId:'',
+            name:'',
+            description:'',
+        }})
         
     }
 
@@ -78,30 +84,30 @@ class SubCatalog extends React.Component {
 
     render(){
         return (
-            <div class="tab-pane fade" id="pills-my-purchases" role="tabpanel">
+            <div className="tab-pane fade" id="pills-my-purchases" role="tabpanel">
                 
-                <div class="table-responsive mt-30">
-                    <div class="panel panel-default" id="accordion">
-                        <div class="panel-heading" role="tab" id="headingOne">
-                            <div class="panel-title adcrse1250">
-                                <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                <div className="table-responsive mt-30">
+                    <div className="panel panel-default" id="accordion">
+                        <div className="panel-heading" role="tab" id="headingOne">
+                            <div className="panel-title adcrse1250">
+                                <a className="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                                     Create New SubCatalog
                                 </a>
                             </div>
                         </div>
                         <div  id="success" style={{display:"none"}}><Success name="Success"/></div>
 
-                        <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-                            <div class="panel-body adcrse_body">
-                                <div class="row">
-                                    <div class="col-lg-8">
-                                        <div class="discount_form">
-                                            <div class="row">
-                                                <div class="col-lg-6 col-md-6">
-                                                    <div class="mt-20 lbel25">	
+                        <div id="collapseOne" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                            <div className="panel-body adcrse_body">
+                                <div className="row">
+                                    <div className="col-lg-8">
+                                        <div className="discount_form">
+                                            <div className="row">
+                                                <div className="col-lg-6 col-md-6">
+                                                    <div className="mt-20 lbel25">	
                                                         <label>Catalog*</label>
                                                     </div>
-                                                    <select class="ui hj145 dropdown cntry152 prompt srch_explore" name="catalogId" onChange={this.handleInputSubCatalogChange}>
+                                                    <select className="ui hj145 dropdown cntry152 prompt srch_explore sub" name="catalogId" onChange={this.handleInputSubCatalogChange}>
                                                         <option value="" selected disabled>Select Catalog</option>
                                                         {
                                                             this.props.catalogs.map((catalog,index) => {
@@ -112,27 +118,27 @@ class SubCatalog extends React.Component {
                                                         }
                                                     </select>
                                                 </div>
-                                                <div class="col-lg-6 col-md-6">	
-                                                    <div class="ui search focus mt-20 lbel25">
+                                                <div className="col-lg-6 col-md-6">	
+                                                    <div className="ui search focus mt-20 lbel25">
                                                         <label>Name</label>
-                                                        <div class="ui left icon input swdh19">
-                                                            <input class="prompt srch_explore" type="text" name="name" required="" placeholder="Name here" onChange={this.handleInputSubCatalogChange}/>															
+                                                        <div className="ui left icon input swdh19">
+                                                            <input className="prompt srch_explore sub" type="text" name="name" required="" placeholder="Name here" onChange={this.handleInputSubCatalogChange}/>															
                                                         </div>
                                                     </div>										
                                                 </div>
-                                                <div class="col-lg-12 col-md-12">	
-                                                    <div class="ui search focus mt-20 lbel25">
+                                                <div className="col-lg-12 col-md-12">	
+                                                    <div className="ui search focus mt-20 lbel25">
                                                         <label>Description</label>
-                                                        <div class="ui form swdh30">
-                                                            <div class="field">
-                                                                <textarea rows="3" name="description" id="" placeholder="Subcatalog description here..." onChange={this.handleInputSubCatalogChange}></textarea>
+                                                        <div className="ui form swdh30">
+                                                            <div className="field">
+                                                                <textarea className="sub" rows="3" name="description" id="" placeholder="Subcatalog description here..." onChange={this.handleInputSubCatalogChange}></textarea>
                                                             </div>
                                                         </div>
-                                                        <div class="help-block">220 words</div>
+                                                        <div className="help-block">220 words</div>
                                                     </div>										
                                                 </div>
-                                                <div class="col-lg-6 col-md-6">	
-                                                    <button class="discount_btn" type="button" value={'AddSubCatalog'} onClick={()=>this.handleSubmitSubCatalog(this.state.addSubCatalog)} >Save Changes</button>										
+                                                <div className="col-lg-6 col-md-6">	
+                                                    <button className="discount_btn" type="button" value={'AddSubCatalog'} onClick={()=>this.handleSubmitSubCatalog(this.state.addSubCatalog)} >Save Changes</button>										
                                                 </div>
                                             </div>
                                         </div>
@@ -153,12 +159,12 @@ class SubCatalog extends React.Component {
                             </div>							
                         </div>							
                     </div>
-                    <table class="table ucp-table">
-                        <thead class="thead-s">
+                    <table className="table ucp-table">
+                        <thead className="thead-s">
                             <tr>
-                                <th class="text-center" scope="col">No.</th>
-                                <th class="cell-ta" scope="col">Name</th>
-                                <th class="text-center" scope="col">Actions</th>
+                                <th className="text-center" scope="col">No.</th>
+                                <th className="cell-ta" scope="col">Name</th>
+                                <th className="text-center" scope="col">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -167,26 +173,26 @@ class SubCatalog extends React.Component {
                                     return (
                                         <tr key={index}>
                                             <td className="text-center">{index + 1}</td>
-                                            <td class="cell-ta">{subCatalog.name}</td>
+                                            <td className="cell-ta">{subCatalog.name}</td>
                                             <td className="text-center"> 
                                                 <a  title="Edit" className="gray-s" data-toggle="modal" data-target={'#idSub'+subCatalog.id} onClick={() => this.getSubCatalog(subCatalog.id, subCatalog.name, subCatalog.description)}><i className="uil uil-edit-alt"></i></a>
                                             </td>
-                                            <div class="modal fade"  tabindex="-1" id={'idSub'+subCatalog.id} aria-hidden="true">
-                                                <div class="modal-dialog modal-lg">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                            <h5 class="modal-title">Edit SubCatalog</h5>
-                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <div className="modal fade"  tabindex="-1" id={'idSub'+subCatalog.id} aria-hidden="true">
+                                                <div className="modal-dialog modal-lg">
+                                                    <div className="modal-content">
+                                                        <div className="modal-header">
+                                                            <h5 className="modal-title">Edit SubCatalog</h5>
+                                                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                                                 <span aria-hidden="true">&times;</span>
                                                             </button>
                                                         </div>
-                                                        <div class="modal-body">
-                                                            <div class="new-section-block">
-                                                                <div class="row">
-                                                                    <div class="col-md-12">
-                                                                        <div class="new-section">
-                                                                            <div class="form_group">
-                                                                                <select class="ui hj145 dropdown cntry152 prompt srch_explore" name="catalogId" onChange={this.handleInputEditSubCatalogChange}>
+                                                        <div className="modal-body">
+                                                            <div className="new-section-block">
+                                                                <div className="row">
+                                                                    <div className="col-md-12">
+                                                                        <div className="new-section">
+                                                                            <div className="form_group">
+                                                                                <select className="ui hj145 dropdown cntry152 prompt srch_explore" name="catalogId" onChange={this.handleInputEditSubCatalogChange}>
                                                                                     <option value="" selected disabled>Select Catalog</option>
                                                                                     {
                                                                                         this.props.catalogs.map((catalog,index) => {
@@ -201,39 +207,39 @@ class SubCatalog extends React.Component {
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                            <div class="new-section-block">
-                                                                <div class="row">
-                                                                    <div class="col-md-12">
-                                                                        <div class="new-section">
-                                                                            <div class="form_group">
-                                                                                <label class="label25">Catalog Name*</label>
-                                                                                <input class="form_input_1" type="text" name="name" defaultValue={subCatalog.name} onChange={this.handleInputEditSubCatalogChange}/>
+                                                            <div className="new-section-block">
+                                                                <div className="row">
+                                                                    <div className="col-md-12">
+                                                                        <div className="new-section">
+                                                                            <div className="form_group">
+                                                                                <label className="label25">Catalog Name*</label>
+                                                                                <input className="form_input_1" type="text" name="name" defaultValue={subCatalog.name} onChange={this.handleInputEditSubCatalogChange}/>
                                                                             </div>
                                                                         </div>
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                            <div class="new-section-block">
-                                                                <div class="row">
-                                                                    <div class="col-md-12">
-                                                                        <div class="new-section">
-                                                                            <div class="form_group">
-                                                                                <label class="label25">Description</label>
-                                                                                <div class="ui form swdh30">
-                                                                                    <div class="field">
+                                                            <div className="new-section-block">
+                                                                <div className="row">
+                                                                    <div className="col-md-12">
+                                                                        <div className="new-section">
+                                                                            <div className="form_group">
+                                                                                <label className="label25">Description</label>
+                                                                                <div className="ui form swdh30">
+                                                                                    <div className="field">
                                                                                         <textarea rows="3" name="description" defaultValue={subCatalog.description} onChange={this.handleInputEditSubCatalogChange}></textarea>
                                                                                     </div>
                                                                                 </div>
-                                                                                <div class="help-block">220 words</div>
+                                                                                <div className="help-block">220 words</div>
                                                                             </div>
                                                                         </div>
                                                                     </div>
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="main-btn cancel" data-dismiss="modal">Close</button>
-                                                            <button type="button" class="main-btn" value={'edit'} data-dismiss="modal" onClick={()=>this.updateSubCatalog(this.state.editSubCatalog)}>Update</button>
+                                                        <div className="modal-footer">
+                                                            <button type="button" className="main-btn cancel" data-dismiss="modal">Close</button>
+                                                            <button type="button" className="main-btn" value={'edit'} data-dismiss="modal" onClick={()=>this.updateSubCatalog(this.state.editSubCatalog)}>Update</button>
                                                         </div>
                                                     </div>
                                                 </div>
